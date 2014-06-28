a3a1b2ebe2e241d6a5ec7ed6947c5940a5ad1f73
luizstacio <luizstacio@gmail.com>
2014-06-28 00:07:58 -0400

    require

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ var app = express();
 var redirects = require('./vhosts.json');
 
 redirects.forEach(function(redirect) {
-  console.log(redirect.host);
-  var app = require(redirect.app);
-  app.use(express.vhost(redirect.host, app));
+  console.log(redirect.app);
+  app.use(express.vhost(redirect.host, require(redirect.app)));
 });
 
 app.listen(3000);
